e576d3ae8c2de777879de53302d6ee1aed23990a
radovanovic-stevan <stevan.radovanovic@grover.com>
2023-02-03 12:51:59 +0100

    Additional form validation

--- src/app/components/job-form/job-form.component.ts
+++ src/app/components/job-form/job-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { JobAdStatus } from 'src/app/interfaces/job-ad.interface';
 
 @Component({
@@ -11,9 +12,19 @@ export class JobFormComponent {
 
   profileForm = new FormGroup({
     title: new FormControl<string>('',[Validators.required]),
-    description: new FormControl<string>('',[Validators.required]),
+    description: new FormControl<string>('',[Validators.required,Validators.minLength(10)]),
     skills: new FormControl<string>('',[Validators.required]),
     status: new FormControl<JobAdStatus | null>(null,[Validators.required])
   });
+
+  constructor(private router: Router) {}
+
+  isFormDisabled() {
+    return this.profileForm.invalid;
+  }
+
+  navigateBack() {
+    this.router.navigateByUrl('ads');
+  }
 
 }
