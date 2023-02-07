209390b74a16e45c9bf63fe720e75dade359e273
radovanovic-stevan <stevan.radovanovic@grover.com>
2023-02-07 15:28:48 +0100

    Fixed status change refetches

--- src/app/state/ads.effects.ts
+++ src/app/state/ads.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, tap, mergeMap, catchError } from 'rxjs/operators';
@@ -23,7 +23,30 @@ export class AdsEffects {
     ofType(updateAd.type),
     mergeMap(({id,changed}: UpdateAdProps) => this.adService.updateAd(id,changed)
       .pipe(
-        map(() => ({ type: updateAdSuccess.type, id,changed, loading: !this.router.url.includes('/ads') })),
+        map(() => {
+          /* Kinda clunky, but this lowers the amount of refetching */
+          const isAdsRoute = this.router.url.includes('/ads');
+          if(!isAdsRoute) {
+            return { type: updateAdSuccess.type, id,changed, loading: true }
+          }
+
+          const queryMap = this.route.snapshot.queryParamMap;
+          const search = queryMap.get('search');
+          const filters = queryMap.get('filters');
+
+          if(search || filters) {
+            return {
+              type: fetchAds.type,
+              searchTerm: search ?? '',
+              filters: filters?.split(',') ?? [],
+              pageSize: +(queryMap.get('pageSize') ?? 1),
+              pageNumber: +(queryMap.get('pageNumber') ?? 5)
+            }
+          }
+
+          return { type: updateAdSuccess.type, id,changed, loading: false }
+
+        }),
         tap(() => this.router.navigate(['ads'], {queryParamsHandling: 'merge'})),
         catchError(() => of({ type: updateAdFail.type}))
       ))
@@ -44,6 +67,7 @@ export class AdsEffects {
   constructor(
     private actions$: Actions,
     private adService: AdService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 }
